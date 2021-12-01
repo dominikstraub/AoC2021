4ef084dc3c6f0c982b22c34b44d493f057b4771a
Dominik Straub <dominik.straub@gmail.com>
2021-12-01 06:26:15 +0100

    Day 01

--- Sources/Day01/main.swift
+++ Sources/Day01/main.swift
@@ -1,8 +1,8 @@
 import Foundation
 import Utils
 
-let input = try Utils.getInput(bundle: Bundle.module, file: "test")
-// let input = try Utils.getInput(bundle: Bundle.module)
+// let input = try Utils.getInput(bundle: Bundle.module, file: "test")
+let input = try Utils.getInput(bundle: Bundle.module)
 
 let lines = input
     .components(separatedBy: CharacterSet(charactersIn: "\n"))
@@ -11,16 +11,42 @@ let lines = input
             return nil
         }
         return line
+    }.map { item -> Int? in
+        Int(item)
     }
+    .compactMap { $0 }
 
 func part1() -> Int {
-    return -1
+    var count = 0
+    var prev: Int?
+    for line in lines {
+        if let prev2 = prev {
+            if line > prev2 {
+                count += 1
+            }
+            prev = line
+        } else {
+            prev = line
+        }
+    }
+    return count
 }
 
 print("Part 1: \(part1())")
 
-// func part2() -> Int {
-//     return -1
-// }
+func part2() -> Int {
+    var count = 0
+    var prev: Int?
+    for index in 2 ... (lines.count - 1) {
+        let current = lines[index] + lines[index - 1] + lines[index - 2]
+        if let prev2 = prev {
+            if current > prev2 {
+                count += 1
+            }
+        }
+        prev = current
+    }
+    return count
+}
 
-// print("Part 2: \(part2())")
+print("Part 2: \(part2())")
