0995e2fa977d468107d8c662f7554f666a6a6121
Dominik Straub <dominik.straub@gmail.com>
2021-12-11 16:27:33 +0100

    Day08 still broken

--- Sources/Day08/main.swift
+++ Sources/Day08/main.swift
@@ -42,6 +42,18 @@ if #available(macOS 12, *) {
     print("Part 1: \(part1())")
 
     func part2() -> Int {
+        // let solution = [8394,
+        //                 9781,
+        //                 1197,
+        //                 9361,
+        //                 4873,
+        //                 8418,
+        //                 4548,
+        //                 1625,
+        //                 8717,
+        //                 4315]
+        let solution = [5353]
+
         // number: 0; segement count: 6; number count: 3
         // number: 1; segement count: 2; number count: 1
         // number: 2; segement count: 5; number count: 3
@@ -54,7 +66,7 @@ if #available(macOS 12, *) {
         // number: 9; segement count: 6; number count: 3
         let allChars: Set<Character> = ["a", "b", "c", "d", "e", "f", "g"]
         var score = 0
-        for line in lines {
+        for (i, line) in lines.enumerated() {
             var possibilities: [Character: Set<Character>] = [
                 "a": ["a", "b", "c", "d", "e", "f", "g"],
                 "b": ["a", "b", "c", "d", "e", "f", "g"],
@@ -65,9 +77,11 @@ if #available(macOS 12, *) {
                 "g": ["a", "b", "c", "d", "e", "f", "g"],
             ]
             for value in line[0] {
+                print(value, terminator: "")
+                print(": ", terminator: "")
                 if value.count == 2 {
                     // number 1
-                    // print(1)
+                    print(1)
                     for char in "cf" {
                         possibilities[char] = possibilities[char]?.intersection(value)
                     }
@@ -76,7 +90,7 @@ if #available(macOS 12, *) {
                     }
                 } else if value.count == 4 {
                     // number 4
-                    // print(4)
+                    print(4)
                     for char in "bcdf" {
                         possibilities[char] = possibilities[char]?.intersection(value)
                     }
@@ -85,7 +99,7 @@ if #available(macOS 12, *) {
                     }
                 } else if value.count == 3 {
                     // number 7
-                    // print(7)
+                    print(7)
                     for char in "acf" {
                         possibilities[char] = possibilities[char]?.intersection(value)
                     }
@@ -93,13 +107,20 @@ if #available(macOS 12, *) {
                         possibilities[char] = possibilities[char]!.subtracting(value)
                     }
                 }
+                print()
             }
             for value in line[0] {
+                print(value, terminator: "")
+                print(": ", terminator: "")
                 let onePosibilities = possibilities["c"]!.union(possibilities["f"]!)
                 let fourPosibilities = possibilities["b"]!.union(possibilities["c"]!).union(possibilities["d"]!).union(possibilities["f"]!)
                 if value.count == 5 {
                     if onePosibilities.count == 2, value.contains(onePosibilities) {
                         // number 3
+                        print(3)
+                        for char in allChars.subtracting("be") {
+                            possibilities[char] = possibilities[char]!.intersection(value)
+                        }
                         for char in "be" {
                             possibilities[char] = possibilities[char]!.subtracting(value)
                         }
@@ -107,16 +128,30 @@ if #available(macOS 12, *) {
                 } else if value.count == 6 {
                     if fourPosibilities.count == 4, value.contains(fourPosibilities) {
                         // number 9
+                        print(9)
+                        for char in allChars.subtracting("e") {
+                            possibilities[char] = possibilities[char]!.intersection(value)
+                        }
                         possibilities["e"] = possibilities["e"]!.subtracting(value)
                     } else if onePosibilities.count == 2, !value.contains(onePosibilities) {
                         // number 6
+                        print(6)
+                        for char in allChars.subtracting("c") {
+                            possibilities[char] = possibilities[char]!.intersection(value)
+                        }
                         possibilities["c"] = possibilities["c"]!.subtracting(value)
                     } else if fourPosibilities.count == 4, onePosibilities.count == 2, value.contains(onePosibilities) {
                         // number 0
+                        print(0)
+                        for char in allChars.subtracting("d") {
+                            possibilities[char] = possibilities[char]!.intersection(value)
+                        }
                         possibilities["d"] = possibilities["d"]!.subtracting(value)
                     }
                 }
+                print()
             }
+            print(possibilities)
             for (segment, segmentPossibilities) in possibilities where segmentPossibilities.count == 1 {
                 for char in allChars.subtracting([segment]) {
                     // elimination
@@ -124,14 +159,17 @@ if #available(macOS 12, *) {
                 }
             }
 
+            print(possibilities)
+
             var decodingTable = [Character: Character]()
             for (segment, segmentPossibilities) in possibilities {
                 decodingTable[segmentPossibilities.first!] = segment
             }
 
-            var localScore = 0
-            for (i, value) in line[1].enumerated() {
-                var val = 0
+            for (i, value) in line[0].enumerated() {
+                print(value, terminator: "")
+                print(": ", terminator: "")
+                var val = -1
                 if value.count == 7 {
                     val = 8
                 } else if value.count == 2 {
@@ -148,7 +186,7 @@ if #available(macOS 12, *) {
                     } else {
                         val = 9
                     }
-                } else {
+                } else if value.count == 5 {
                     if value.contains([decodingTable["b"]!, decodingTable["f"]!]) {
                         val = 5
                     } else if value.contains(decodingTable["f"]!) {
@@ -156,12 +194,51 @@ if #available(macOS 12, *) {
                     } else {
                         val = 2
                     }
+                } else {
+                    val = -1
+                }
+                print(val)
+            }
+
+            var localScore = 0
+            for (i, value) in line[1].enumerated() {
+                print(value, terminator: "")
+                print(": ", terminator: "")
+                var val = -1
+                if value.count == 7 {
+                    val = 8
+                } else if value.count == 2 {
+                    val = 1
+                } else if value.count == 4 {
+                    val = 4
+                } else if value.count == 3 {
+                    val = 7
+                } else if value.count == 6 {
+                    if !value.contains(decodingTable["d"]!) {
+                        val = 0
+                    } else if !value.contains(decodingTable["c"]!) {
+                        val = 6
+                    } else {
+                        val = 9
+                    }
+                } else if value.count == 5 {
+                    if value.contains([decodingTable["b"]!, decodingTable["f"]!]) {
+                        val = 5
+                    } else if value.contains(decodingTable["f"]!) {
+                        val = 3
+                    } else {
+                        val = 2
+                    }
+                } else {
+                    val = -1
                 }
 
+                print(val)
                 localScore += val * 10 ^^ (line[1].count - i - 1)
             }
-            print(localScore)
+            print("\(localScore), \(solution[i]), \(localScore == solution[i])")
             score += localScore
+            return score
         }
         return score
     }
